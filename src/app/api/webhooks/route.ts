a975d31bd61871707e8eea4fd3ef2620706fd7c3
prabhat <prabhatshukla0849@gmal.com>
2024-06-14 16:47:09 +0530

    updated build

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -72,7 +72,7 @@ export async function POST(req: Request) {
             })
 
             await resend.emails.send({
-                from: 'CaseCobra <prabhatshukla0849@gmail.com.com>',
+                from: 'CaseCobra <prabhatshukla0849@gmail.com>',
                 to: [event.data.object.customer_details.email],
                 subject: 'Thanks for your order!',
                 react: OrderReceivedEmail({
